6fdcb085eac26cf4d6e0355a5fb865d52cfff625
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2019-10-29 17:17:05 -0700

    Override for == implemented

--- sound_byte/lib/user.dart
+++ sound_byte/lib/user.dart
@@ -1,5 +1,7 @@
 class User
 {
+  // UserIDs will be in the format userName#xxxx
+  // Where xxxx is a 4 digit int
   String userID;
   
   User(String username, int id)
@@ -7,15 +9,23 @@ class User
     this.userID = username + "#" + id.toString();
   }
 
+  // Returns only the 4 digit int
   int getUnquieNum()
   {
     List<String> parts = userID.split("#");
     return int.parse(parts[1]);
   }
 
+  // Returns only the userName
   String getUserName()
   {
     List<String> parts = userID.split("#");
     return parts[0].substring(0, parts[0].length-2);
+  }
+
+  @override
+  bool operator ==(Object other)
+  {
+    return other is User && this.userID == other.userID;
   }
 }
