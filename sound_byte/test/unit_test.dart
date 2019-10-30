fe3f319ab65581a4b546345afdd653053d64db27
joesobo <soboleskijoe@gmail.com>
2019-10-29 18:25:47 -0700

    added unit test example

---
+++ sound_byte/test/unit_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:sound_byte/main.dart';
+
+void main() {
+  test('Testing tests', () {
+    MyApp app = new MyApp();
+
+    expect(app.testFunction(), true);
+  });
+}
