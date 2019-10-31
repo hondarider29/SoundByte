439a51abb4f097fe1db7a66ddadb699d8d9cb02f
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2019-10-30 19:51:08 -0700

    fixed deleted code

--- sound_byte/ios/Runner/AppDelegate.swift
+++ sound_byte/ios/Runner/AppDelegate.swift
@@ -13,3 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
+
+
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
