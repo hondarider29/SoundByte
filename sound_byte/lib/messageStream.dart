21ae05c827ec0c8a8497e4c7378a429d14f395ae
Joe Soboleski <26047842+joesobo@users.noreply.github.com>
2019-11-02 11:37:39 -0700

    still rebasing

---
+++ sound_byte/lib/messageStream.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sound_byte/messageBubble.dart';
+
+final _firestore = Firestore.instance;
+//FirebaseUser loggedInUser;
+
+class MessagesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      //TODO: change firestore collection to be actual ones. Here lie demos
+      stream:_firestore.collection('messages').orderBy('timestamp').snapshots(),
+      builder: (context, snapshot) {
+        //loading screen
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        //load messages
+        } else {
+          //pull messages in reverse so newest come first
+          final messages = snapshot.data.documents.reversed;
+          List<MessageBubble> messageBubbles = [];
+          for (var message in messages) {
+            final messageText = message.data['text'];
+            final senderText = message.data['sender'];
+            final timeStamp = message.data['timestamp'];
+
+            //final currentUser = loggedInUser.email;
+
+            //create a new message bubble widget to load
+            messageBubbles.add(
+              MessageBubble(
+                text: messageText,
+                sender: senderText,
+                timeStamp: timeStamp,
+                //isMe: currentUser == senderText,
+              ),
+            );
+          }
+
+          //actual displaying of the messages
+          return Expanded(
+            child: ListView(
+              reverse: true,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              children: messageBubbles,
+            ),
+          );
+        }
+      },
+    );
+  }
+}
