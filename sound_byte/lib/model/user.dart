79d5d4778f81efa55ab03e303013637747e641fd
hondarider29 <>
2019-12-03 17:03:52 -0800

    Implemented usersFromDataBase

--- sound_byte/lib/model/user.dart
+++ sound_byte/lib/model/user.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 
 class User
@@ -16,6 +18,26 @@ class User
   {
     this.userID = username + "#"  + this.formatter.format(id);
     this.userEmail = email;
+  }
+
+  User.full(String userID, String userEmail, List<String> friends, List<String> conversations)
+  {
+    this.userID = userID;
+    this.userEmail = userEmail;
+    this.friends = friends;
+    this.conversations = conversations;
+  }
+
+  User userFromDatabase(String userEmail)
+  {
+    User user;
+    Firestore.instance.collection('users').where('email', isEqualTo: userEmail).getDocuments()
+      .then((documentSnapshot) => user = new User.full(documentSnapshot.documents[0].data['userID'],
+                                                      documentSnapshot.documents[0].data['email'],
+                                                      documentSnapshot.documents[0].data['friends'],
+                                                      documentSnapshot.documents[0].data['conversations'])
+        );
+    return user;
   }
 
   // Returns only the 4 digit int
