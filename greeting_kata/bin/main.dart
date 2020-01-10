30f29dc7b2b75979e53cbead7671a6ad25123da3
GMOkeefe <35286980+GMOkeefe@users.noreply.github.com>
2020-01-10 15:01:50 -0800

    created new dart project

---
+++ greeting_kata/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:greeting_kata/greeting_kata.dart' as greeting_kata;
+
+main(List<String> arguments) {
+  print('Hello world: ${greeting_kata.calculate()}!');
+}
---
+++ greeting_kata/test/greeting_kata_test.dart
@@ -0,0 +1,8 @@
+import 'package:greeting_kata/greeting_kata.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
