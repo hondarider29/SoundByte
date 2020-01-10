2ce917a5b3af6180e878fb6c35b5e59a96f1b2c6
sannd22 <davidsann92@gmail.com>
2020-01-10 16:46:59 -0500

    Add files via upload

---
+++ sound_byte/lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:sound_byte/pages/friendScreen.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.red,
+      home: PageView(
+        children: <Widget> [
+          FriendScreen(),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+}
