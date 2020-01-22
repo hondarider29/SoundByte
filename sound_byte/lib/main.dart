b668473a56e4c6c764de590b527a04dc1ca519ee
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2020-01-22 15:50:35 -0800

    updated main

--- sound_byte/lib/main.dart
+++ sound_byte/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sound_byte/services/authentication.dart';
 import 'package:sound_byte/pages/root_page.dart';
-import 'package:sound_byte/loginScreen.dart';
-import 'package:sound_byte/chatScreen.dart';
-import 'package:sound_byte/homePage.dart';
 
 void main() {
   runApp(MyApp());
