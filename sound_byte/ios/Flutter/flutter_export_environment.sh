e187cbe7905541d4595f77b6e891dd67bfbf8826
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-01-23 15:35:37 -0800

    added logout to userPage, not working

--- sound_byte/ios/Flutter/flutter_export_environment.sh
+++ sound_byte/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/cayalame/Desktop/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/cayalame/Desktop/SoundByte/SoundByte/sound_byte"
-export "FLUTTER_TARGET=/Users/cayalame/Desktop/SoundByte/SoundByte/sound_byte/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/cayalame/Desktop/sprint4/SoundByte/sound_byte"
+export "FLUTTER_TARGET=/Users/cayalame/Desktop/sprint4/SoundByte/sound_byte/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/cayalame/Desktop/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "TRACK_WIDGET_CREATION=true"
