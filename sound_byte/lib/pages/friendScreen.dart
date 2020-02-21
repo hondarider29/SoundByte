654ffc57ef34470d74cb2adcfcb8d91400c54185
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-02-21 13:41:20 -0800

    Update friendScreen.dart

--- sound_byte/lib/pages/friendScreen.dart
+++ sound_byte/lib/pages/friendScreen.dart
@@ -96,15 +96,15 @@ class _FriendScreenState extends State<FriendScreen> {
               //friend list
               //TODO: add real data
               friendButton(
-                  'images/headShot1.jpeg', "John", "last online: 2 hours ago", 1, "Software Engineer"),
+                  'images/headShot1.jpeg', "John", "last online: 2 hours ago", "user", "Software Engineer"),
               friendButton(
-                  'images/headShot2.jpeg', "David", "Online", 2 ,"Soccer Player"),
+                  'images/headShot2.jpeg', "David", "Online", "USER ID" ,"Soccer Player"),
               friendButton(
-                  'images/headShot3.jpeg', "Xavier", "last online: 3 minutes ago", 3, "Teacher"),
+                  'images/headShot3.jpeg', "Xavier", "last online: 3 minutes ago", "USER ID", "Teacher"),
               friendButton(
-                  'images/headShot4.jpeg', "Sarah", "last online: 4 seconds ago", 4, "Nurse"),
+                  'images/headShot4.jpeg', "Sarah", "last online: 4 seconds ago", "USER ID", "Nurse"),
               friendButton(
-                  'images/headShot5.png', "Jennifer", "Online", 5, "Lawyer")
+                  'images/headShot5.png', "Jennifer", "Online", "USER ID", "Lawyer")
             ],
           ),
         ),
@@ -113,7 +113,7 @@ class _FriendScreenState extends State<FriendScreen> {
   }
 
   //creates a button displaying all the information about a friend
-  Widget friendButton(String imageName, String name, String subText, int id, String status) {
+  Widget friendButton(String imageName, String name, String subText, String id, String status) {
     double size = 50;
 
     return Column(
