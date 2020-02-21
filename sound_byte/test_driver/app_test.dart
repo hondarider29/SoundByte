9f0f16615da2d9f9678ae8bd113dd00582720227
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-02-21 14:45:14 -0800

    Add files via upload

---
+++ sound_byte/test_driver/app_test.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:glob/glob.dart';
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'steps/steps.dart';
+
+Future<void> main() {
+  final config = FlutterTestConfiguration()
+    ..features = [Glob(r"test_driver/features/**.feature")]
+    ..stepDefinitions = [LoginValidation(), PasswordValidation(), LoginButton(), Nav_Validation()]
+    ..reporters = [ProgressReporter(), TestRunSummaryReporter()]
+    ..restartAppBetweenScenarios = true
+    ..targetAppPath = "test_driver/app.dart"
+    ..exitAfterTestRun = true;
+  return GherkinRunner().execute(config);
+}
