70542e7fe7fd620b11f4a684803c50090c7a5325
Rohan Ramani <rramani1230@gmail.com>
2020-02-26 11:39:47 -0800

    Beggining of the snapkit Integration

--- sound_byte/lib/pages/friendList.dart
+++ sound_byte/lib/pages/friendList.dart
@@ -5,3 +5,37 @@ import 'package:sound_byte/pages/userProfile.dart';
 import 'package:sound_byte/pages/friendProfile.dart';
 import 'package:sound_byte/pages/friendScreen.dart';
 
+
+class FriendList extends StatelessWidget{
+  
+  User friend;
+  String nameOfList;
+  int friendsListId;
+  int numberOfFriends;
+  FriendList listOfFriends;
+
+  FriendsList(int numberOfFriends, FriendList listOfFriends, String nameOfList, int friendsListId) {
+    this.numberOfFriends = numberOfFriends;
+    this.listOfFriends = listOfFriends;
+    this.nameOfList = nameOfList;
+    this.friendsListId = friendsListId;
+  }
+
+  Widget _buildFriendsList (User friend) {
+    print(friend.username);
+    print(friend.id);
+    return Center (
+      child: Container (
+        width: 500.0,
+        height: 70,
+        decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(500.0),
+        border: Border.all(
+          color: Colors.red,
+          width: 100.0
+        ),
+      ),
+    )
+    );
+  }
+}
