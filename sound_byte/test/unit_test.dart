6ecd2ac86eebb874833fee835e035356be7c866d
hondarider29 <26152348+hondarider29@users.noreply.github.com>
2020-03-04 14:18:19 -0800

    added new unit test

--- sound_byte/test/unit_test.dart
+++ sound_byte/test/unit_test.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:sound_byte/main.dart';
+import 'package:sound_byte/model/user.dart';
 
 void main() {
   test('Testing tests', () {
-    MyApp app = new MyApp();
-
    expect(true, true);
+  });
+
+  test('Testing new User', () {
+    User test = new User('unittester', 'unittest@1.non', 'unittester01');
+
+    expect(test.userEmail, 'unittest@1.non');
+    expect(test.userName, 'unittester');
+    expect(test.userID, 'unittester01');
+    expect(test.friends, new List<String>());
+    expect(test.chats, new List<String>());
   });
 }
