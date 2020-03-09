61d145b3dc1121bd0ccce37a58622bdcd0e474f5
GMOkeefe <35286980+GMOkeefe@users.noreply.github.com>
2020-03-09 14:32:02 -0700

    changed some functionality of messageEncrypter and added unit tests

--- sound_byte/test/unit_test.dart
+++ sound_byte/test/unit_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sound_byte/main.dart';
+import 'package:sound_byte/helperClasses/messageEncrypter.dart';
 import 'package:sound_byte/model/user.dart';
 import 'package:sound_byte/pages/Songs.dart';
 
@@ -55,5 +56,56 @@ void main() {
     expect(s.title, "Star Spangled Banner");
     expect(s.artist, "Francis Scott Key");
     expect(s.image, "img1");
+  });
+
+  test('Creating new MessageEncrypter Pair', () {
+    final password1 = 'password';
+    final password2 = 'pass1234';
+
+    var m1 = new MessageEncrypter(password1);
+    var m2 = new MessageEncrypter.forReceiver(
+      password2, m1.textKey, m1.ivText);
+
+    expect(m1.textKey, m2.textKey);
+    expect(m1.passKey, isNot(m2.passKey));
+    expect(m1.ivText, m2.ivText);
+  });
+
+  test('Encrypting a message', () {
+    final password = 'password';
+    final message = 'message';
+    
+    var encrypter = MessageEncrypter(password);
+    var cipher = encrypter.encrypt(message);
+
+    expect(encrypter.decrypt(cipher), message);
+  });
+
+  test('Sending a message', () {
+    final password1 = 'password';
+    final password2 = 'pass1234';
+    final message = 'message';
+
+    var encrypter = MessageEncrypter(password1);
+    var decrypter = MessageEncrypter.forReceiver(
+      password2, encrypter.textKey, encrypter.ivText);
+    var cipher = encrypter.encrypt(message);
+    var received = decrypter.decrypt(cipher);
+
+    expect(message, received);    
+  });
+
+  test('Storing key data', () {
+    final password = 'password';
+    
+    var m = MessageEncrypter(password);
+    print(m.passkey.stretch(32).base64.length);
+    print(m.passKey.length);
+    var data = m.encodeData();
+    var mRehyd = MessageEncrypter.rehydrate(password, data);
+
+    expect(m.textKey, mRehyd.textKey);
+    expect(m.passKey, mRehyd.passKey);
+    expect(m.ivText, mRehyd.ivText);
   });
 }
